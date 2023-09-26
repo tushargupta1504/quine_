2cdb6dbe233cd717a2605a96a6da4758248bda9b
tushargupta1504 <138330699+tushargupta1504@users.noreply.github.com>
2023-09-26 16:36:40 +0530

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num1;
+  cout<<"Enter the first number here: ";
+    cin>>num1;
+
+    int num2;
+  cout<<"Enter the second number here: ";
+    cin>>num2;
+    
+    int sum=num1+num2;
+    int dif= num1 - num2;
+    int mul= num1*num2;
+    int div= num1/num2;
+    cout<<sum<<endl;
+    cout<<dif<<endl;
+    cout<<mul<<endl;
+    cout<<div;
+
+}
